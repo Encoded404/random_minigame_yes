ec90df6e36736d429937e3c6e2bbd5610a8fcbad
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2023-04-22 10:47:11 +0200

    blend filrd

---
+++ Assets/Script/TerminalScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+        
+public class TerminalScript : MonoBehaviour
+{
+    private List<string> lines;
+    public TMP_InputField input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        input.Select();
+        input.ActivateInputField();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input.Select();
+        input.ActivateInputField();
+
+    }
+    void UpdateScreen(lines) {
+        
+    }
+}
