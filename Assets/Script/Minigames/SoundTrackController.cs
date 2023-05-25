67b13bb33018b14dbe38e082dcdb6d2eedf4ff16
NTh1nk <83032430+NTh1nk@users.noreply.github.com>
2023-05-25 14:10:43 +0200

    works

--- Assets/Script/Minigames/SoundTrackController.cs
+++ Assets/Script/Minigames/SoundTrackController.cs
@@ -13,10 +13,24 @@ public class SoundTrackController : MonoBehaviour
         get { return GetComponent<Slider>(); }
     }
     public AudioMixer mixer;
+    [SerializeField]
+    private Text volumeLabel;
+
+    private void Start()
+    {
+        UpdateValueOnChange(slider.value);
+        slider.onValueChanged.AddListener(delegate
+        {
+            UpdateValueOnChange(slider.value);
+        });
+    }
     public string volumeName;
 
     public void UpdateValueOnChange(float value)
     {
-        mixer.SetFloat(volumeName, value);
+        if(mixer != null) 
+        mixer.SetFloat(volumeName, Mathf.Log(value) * 20f);
+        if (volumeLabel != null)
+            volumeLabel.text = Mathf.Round(value * 100.0f).ToString() + "%";
     }
 }
