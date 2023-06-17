35d418a114ca8105166e1de54992c2392b0151d8
NTh1nk <83032430+NTh1nk@users.noreply.github.com>
2023-06-17 13:12:17 +0200

    Stuff

---
+++ Assets/SceneHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        SceneManager.LoadScene("SpaceInvadersMinigame", LoadSceneMode.Additive);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
