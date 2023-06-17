8b2db046e0afb98b8878daa536a71a3cf5a8a120
NTh1nk <83032430+NTh1nk@users.noreply.github.com>
2023-06-17 15:41:24 +0200

    Works

--- Assets/Script/Minigames/EscMenu.cs
+++ Assets/Script/Minigames/EscMenu.cs
@@ -3,13 +3,19 @@ using UnityEngine;
 public class EscMenu : MonoBehaviour
 {
     public GameObject objectToToggle;
+    public Animator animator;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
 
+    }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
+        if (Input.GetKeyDown(KeyCode.Escape)) { 
+   
             // Toggle the active state of the object
             objectToToggle.SetActive(!objectToToggle.activeSelf);
+           
         }
     }
 }
